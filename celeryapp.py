6313fc4d5865b4d47efa6b6a099992abaa4d6b6e
Ola <>
2023-05-04 11:49:34 +0000

    fthhjh

--- celeryapp.py
+++ celeryapp.py
@@ -1,8 +1,8 @@
 from celery import Celery
 from mail import send_email
 app = Celery('proj',
-             broker='redis://redis:6379/0',
-             backend='redis://redis:6379/0',
+             broker='redis://127.0.0.1:6379/0',
+             backend='redis://127.0.0.1:6379/0',
              )
 
 # Optional configuration, see the application user guide.
@@ -13,8 +13,8 @@ app = Celery('proj',
 
 
 @app.task
-def send_email_t(meeting):
-    send_email(meeting)
+async def send_email_t(meeting):
+    await send_email(meeting)
 
 
 
